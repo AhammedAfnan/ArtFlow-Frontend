1276d4c4f980f8a1741b224303ec9dd70e191bb5
afnan <afnanballoor44@gmail.com>
2024-11-23 13:58:56 +0530

    solved api issue

--- src/Helper/instance.js
+++ src/Helper/instance.js
@@ -1,14 +1,6 @@
-// to make HTTP requests 
-// keep request and response hadnling logic clean and centralized
-
-import URL from '../config/api'
+import BASE_URL from '../config/api'
 import axios from 'axios';
-const user = axios.create({baseURL:URL.BASE_URL})
-
-// eente story enganen chelyeng, aaran user oo admin oo artist oo aaran oork ille access kodkan . ippo userRequest nte ullilek banneng
-// register cheythappo create aakiye token user.defaul..... eel bekkum . ennit error onnu illang .. onsuccess variable k option aayt nammmo
-// edthe arg ade pole response aayt variable bekkum. error engil error aa error variable l bekkum . ennit last ad ansarichitt return cheyyu 
-// user request bilchedthekanne
+const user = axios.create({baseURL:BASE_URL})
 
 export const userRequest = async ({ ...options }) => {
     //the Authorization header
